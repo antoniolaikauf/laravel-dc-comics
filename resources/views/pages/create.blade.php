7476676e6bb4a71e4f7a7323952c07452e8c7509
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-15 14:10:10 +0100

    add:creazione del form in file create.blade

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -5,5 +5,25 @@
 @section('content')
 <h1>Hello, World!</h1>
 
-
+<form action="">
+    <div class="my-3">
+        <label for="name-comic">nome comic</label>
+        <div>
+            <input type="text" name="name" id="name-comic">
+        </div>
+    </div>
+    <div class="my-3">
+        <label for="data">data della sua uscita</label>
+        <div>
+            <input type="text" name="data-uscita" id="data">
+        </div>
+    </div>
+    <div class="my-3">
+        <label for="voto">voto</label>
+        <div>
+            <input type="text" name="voto" id="voto">
+        </div>
+    </div>
+    <input type="button" value="crea">
+</form>
 @endsection
